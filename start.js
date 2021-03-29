3ef4b9c57e910d5ee4ccf20ff04f0a8201be4aba
Mxx1029 <lia.pim@icloud.com>
2021-03-29 13:41:54 +0200

    Initial import

---
+++ start.js
@@ -0,0 +1,25 @@
+console.log("this is an exercise in console logging");
+
+const value = "This is the value.";
+
+console.log(value);
+
+const firstName = "Maxie";
+
+const lastName = "Haufe";
+
+console.log(firstName, lastName);
+
+const userName = "Mxx1029";
+
+const city = "Berlin";
+
+const favoriteColor = "pink";
+
+const age = "34";
+
+const nativeLanguage = "German";
+
+console.log(userName, city, favoriteColor, age, nativeLanguage);
+
+console.log(userName + "'s native language is " + nativeLanguage + " and they are a resident of " + city + ". Their favorite color is " + favoriteColor + " and they are currently " + age + " years old. Everybody say Welcome to " + userName + "!");
